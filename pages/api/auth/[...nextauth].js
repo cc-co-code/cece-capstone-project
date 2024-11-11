db85bd705586e2bacb050857ae600adf71d83efb
YOUR NAME <cecilia@bartlewski.de>
2024-11-11 17:41:13 +0100

    fixed community button link for login redirection

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,10 @@
-import nextAuth from "next-auth";
+import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import dbConnect from "@/lib/mongodb";
 import GoogleProvider from "next-auth/providers/google";
 
-export default nextAuth({
+export default NextAuth({
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
